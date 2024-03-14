efda93db0ab8c54b5cb83d753b700041dc7316c7
alfred@soidea.co <alfred@soidea.co>
2024-03-14 10:45:21 +0700

    fix lark

--- app.js
+++ app.js
@@ -117,6 +117,7 @@ app.post('/line/webhook/:forcompany', async (req, res) => {
       }
     })
     let responsecreate = await functionjs.create_userline(thisforcompany, userId, thisstoken)
+    /edit-larkchat/:forcompany/:userId/:larkchatid
     console.log(responsecreate)
     let responsequery = await functionjs.query_message_by_user(thisstoken, thisforcompany , userId)
     console.log(responsequery)
@@ -124,30 +125,30 @@ app.post('/line/webhook/:forcompany', async (req, res) => {
   }
 })
 
-app.post('/lark/webhook/:forcompany', async (req, res) => {
-  let thisforcompany,thisstokenres
-  let thisparam = req.params.forcompany
-  let requestbody = req.body
-  console.log(JSON.stringify(req.body))
-  if (requestbody.type == "url_verification") {
-    if (requestbody.challenge) {
-      res.status(200).send({ "challenge": requestbody.challenge })
-    }
-  } else {
-    let messageraw = requestbody['event']
-    let resuser = await functionjs.get_userline_data_larkchat(thisforcompany, , thisstoken)
-    addDoc(collection(dbstore, "message_lark_"+thisparam), {
-      init_timestamp: currentElement.timestamp,
-      user_id: resuser.user_id,
-      message_data: messageraw.message,
-      status: "wait",
-      forcompany: thisparam,
-      timestamp: serverTimestamp(),
-      created_at: Date.now()
-    });
-    res.status(200).send("ok")
-  }
-})
+// app.post('/lark/webhook/:forcompany', async (req, res) => {
+//   let thisforcompany,thisstokenres
+//   let thisparam = req.params.forcompany
+//   let requestbody = req.body
+//   console.log(JSON.stringify(req.body))
+//   if (requestbody.type == "url_verification") {
+//     if (requestbody.challenge) {
+//       res.status(200).send({ "challenge": requestbody.challenge })
+//     }
+//   } else {
+//     let messageraw = requestbody['event']
+//     let resuser = await functionjs.get_userline_data_larkchat(thisforcompany, , thisstoken)
+//     addDoc(collection(dbstore, "message_lark_"+thisparam), {
+//       init_timestamp: currentElement.timestamp,
+//       user_id: resuser.user_id,
+//       message_data: messageraw.message,
+//       status: "wait",
+//       forcompany: thisparam,
+//       timestamp: serverTimestamp(),
+//       created_at: Date.now()
+//     });
+//     res.status(200).send("ok")
+//   }
+// })
 
 app.post('/line-checkdata/:forcompany', async (req, res) => {
   let thisparam = req.params.forcompany
